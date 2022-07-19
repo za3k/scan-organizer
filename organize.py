6363c1386c0cb16c8b9c8e3f6164e1102565868d
Zachary Vance <za3k@za3k.com>
2022-07-19 00:55:43 -0400

    Cleanup
    Clean up constant use of self.blah[phase] by adding self.phases() and self.phase_info()
    Clarify self.phase_index when the working set is size zero.
    Fix two vague method uses case. Rewrite _update_image as set_image. reload_image is not internal any more. Use cases are overall clearer for both.
    Reduce code length by using defaultdict in place of phase initialization
    Don't pass around the master path. Remove it from Organizer.

--- organize.py
+++ organize.py
@@ -7,30 +7,24 @@
 # Phase 5 [+hand_transcribe -transcribed]: Transcribe files by hand. +transcribed
 
 import ui
-import frontmatter, functools, natsort, os, pathlib, subprocess, sys
+import collections, frontmatter, functools, natsort, os, pathlib, subprocess, sys
 
 class OrganizerCategory():
-    def __init__(self, path, parent):
+    def __init__(self, path, name):
         self.path = path
-        self.parent = parent
-    def relative_path(self):
-        return self.path.relative_to(parent)
+        self.name = name
 
 class OrganizerImage():
-    def __init__(self, path, parent, categories):
+    def __init__(self, path, category, index):
         self.image_path = path
-        self.parent = parent
         self.transcription_path = self._transcription_path(self.image_path)
+        self.index = index # an id
+        self.category = category
         if self.transcription_path.exists():
             self.textfm = frontmatter.load(self.transcription_path)
         else:
             self.textfm = frontmatter.Post("")
             self.textfm['tags'] = []
-        self.category = None
-        for category in categories:
-            if str(self.image_path).startswith(str(category.path)):
-                if self.category is None or len(category.path) > len(self.category.path):
-                    self.category = category
     def rename(self, new_name):
         self._move(self.image_path.parent.joinpath(new_name))
     def set_category(self, category):
@@ -50,30 +44,26 @@ class OrganizerImage():
         """Add or remove a single tag"""
         assert any(tag.startswith(x) for x in "+-")
         symbol, tag = tag[:1], tag[1:]
-        if symbol == "+":
-            if tag not in self.tags:
-                self.tags.append(tag)
-        elif symbol == "-":
-            if tag in self.tags:
-                self.tags.remove(tag)
+        if symbol == "+" and tag not in self.tags:
+            self.tags.append(tag)
+        elif symbol == "-" and tag in self.tags:
+            self.tags.remove(tag)
         self._save_text()
     def match_tags(self, tags):
         """Returns true if ALL tags are matched"""
         for tag in tags:
-            if tag.startswith("+"):
-                if tag[1:] not in self.tags:
-                    return False
-            elif tag.startswith("-"):
-                if tag[1:] in self.tags:
-                    return False
-            else:
-                assert False
+            assert any(tag.startswith(x) for x in "+-")
+            symbol, tag = tag[:1], tag[1:]
+            if symbol == "+" and tag not in self.tags:
+                return False
+            elif symbol == "-" and tag in self.tags:
+                return False
         return True
 
     def _save_text(self):
         self.textfm['filename'] = self.image_path.name
         if self.category is not None:
-            self.textfm['category'] = self.category.relative_path
+            self.textfm['category'] = self.category.name
         frontmatter.dump(self.textfm, self.transcription_path)
     def _move(self, new_path):
         old_image_path, self.image_path = self.image_path, new_path
@@ -90,10 +80,15 @@ class Organizer():
         self.window = ui.TranscriptionWindow()
         self.images = []
         self.categories = []
-        self.phase_images = {}
-        self.phase_work_images = {}
-        self.phase_tags = {}
-        self.phase_index = {}
+        self._phases = []
+        # phase -> All images for that phase, at least those unfinished at the program start. As indices
+        self._phase_images = collections.defaultdict(list)
+        # phase -> Images still requiring work. As indices
+        self._phase_work_images = collections.defaultdict(list)
+        # phase -> list of tags
+        self._phase_tags = {}
+        # phase -> current selected image
+        self._phase_index = collections.defaultdict(lambda: None)
 
         self.add_phase(
             name="Phase 1: Clean",
@@ -155,155 +150,156 @@ class Organizer():
         files = []
         dirs = [master]
         for p in dirs:
-            #print(p)
             assert p.is_dir()
             for x in p.iterdir():
-                #print(x)
                 if x.is_file():
                     files.append(x)
                 elif x.is_dir():
                     dirs.append(x)
-        files = [f for f in files if f.suffix in {".png", ".jpg", ".jpeg", ".tiff", ".tif", ".gif"}]
-        categories = [d.relative_to(master) for d in dirs]
 
-        for category in categories:
-            organizer.add_category(category, master)
+        for category in dirs:
+            organizer.add_category(category, str(category.relative_to(master)))
         for file in files:
-            organizer.add_image(file, master)
+            if file.suffix in {".png", ".jpg", ".jpeg", ".tiff", ".tif", ".gif"}:
+                organizer.add_image(file)
         organizer.autoselect_phase()
     def add_phase(self, tags, **kwargs):
         phase = self.window.add_phase(**kwargs)
-        self.phase_tags[phase] = tags
-        self.phase_index[phase] = 0
-        self.phase_images[phase] = [] # All of them added from the beginning
-        self.phase_work_images[phase] = [] # The ones remaining that need work only, as indexes
-        # Images are always added after phases, so skip any stuff to add them here.
-    def add_image(self, image_path, parent):
-        image = OrganizerImage(image_path, parent, self.categories)
+        self._phases.append(phase)
+        self._phase_tags[phase] = tags
+        # Images are always added after phases, so skip iterating over existing images
+    def set_phase_index(self, phase, index):
+        self._phase_index[phase] = index
+    def phase_info(self, phase):
+        return self._phase_tags[phase], self._phase_index[phase], self._phase_images[phase], self._phase_work_images[phase]
+    def phases(self):
+        for phase in self._phases:
+            yield phase, *self.phase_info(phase)
+    def add_image(self, image_path):
+        image = OrganizerImage(image_path, self._find_category(image_path), index=len(self.images))
         self.images.append(image)
 
-        for phase, tags in self.phase_tags.items():
+        for phase, tags, phase_index, images, work_images in self.phases():
             if image.match_tags(tags):
-                self.phase_images[phase].append(image)
-                self.phase_work_images[phase].append(len(self.phase_images[phase])-1)
+                images.append(image.index)
+                work_images.append(image.index)
                 phase.increment_todo(1)
-                if self.phase_index[phase] == 0 and len(self.phase_images[phase]) == 1: # First image
-                    self._update_image(phase)
+                if phase_index is None:
+                    self.set_image(phase, 0)
             else:
                 phase.increment_skipped(1)
-    def add_category(self, category_path, parent):
-        category = OrganizerCategory(category_path, parent)
+    def add_category(self, category_path, category_name):
+        category = OrganizerCategory(category_path, category_name)
         self.categories.append(category)
     def display(self):
         self.window.mainloop()
     def autoselect_phase(self):
         best_phase = None
-        for phase, items in reversed(self.phase_work_images.items()):
-            if len(items) == 0:
-                phase.set_done(True)
-            else:
+        for phase, _, _, _, work_images in reversed(list(self.phases())):
+            if len(work_images) > 0:
                 best_phase = phase
         if best_phase is not None:
             self.window.select_phase(best_phase)
-    def _switch_index(self, phase, offset):
-        current_index = self.phase_index[phase]
-
-        if len(self.phase_images[phase]) == 0:
-            new_index = 0
+    def set_image(self, phase, new_index):
+        """Use if the selected image changed for a phase"""
+        self.set_phase_index(phase, new_index)
+        tags, phase_index, images, work_images = self.phase_info(phase)
+        if len(images) == 0:
+            phase.set_done(True)
+            phase.set_image(None, False)
         else:
-            new_index = (current_index + offset) % len(self.phase_images[phase])
+            phase.set_done(False)
+            assert phase_index in images
+            image = self.images[phase_index]
+            is_work = phase_index in work_images
+            phase.set_image(image, is_work)
+    def reload_image(self, image):
+        """Use if we think an image was changed externally"""
+        for phase, tags, phase_index, images, work_images in self.phases():
+            if len(images) > 0 and phase_index == image.index:
+                is_work = image.index in work_images
+                phase.set_image(image, is_work)
 
-        self.phase_index[phase] = new_index
-        self._update_image(phase)
-    def _switch_work_index(self, phase, offset, working_set):
-        current_index = self.phase_index[phase]
+    def _switch_index(self, phase, offset, working_set):
+        _, current_index, _, _ = self.phase_info(phase)
         if len(working_set) == 0:
-            new_index = 0
+            new_index = None
         else:
-            # Next WORK image. Assumes we're on one. Should fix that, eventually
+            # Next image. Assumes we're on one. Should fix that, eventually
             assert current_index in working_set
             work_index = working_set.index(current_index)
             next_work_index = (work_index + offset) % len(working_set)
             new_index = working_set[next_work_index]
 
-        self.phase_index[phase] = new_index
-        self._update_image(phase)
-    def _update_image(self, phase):
-        index = self.phase_index[phase]
-        if len(self.phase_images[phase]) == 0:
-            phase.set_done(True)
-            phase.set_image(None, False)
-        else:
-            phase.set_done(False)
-            assert 0 <= index < len(self.phase_images[phase])
-            image = self.phase_images[phase][index]
-            is_work = index in self.phase_work_images[phase]
-            phase.set_image(image, is_work)
-    def _reload_image(self, image):
-        """Use if we think an image was changed externally"""
-        for phase, index in self.phase_index.items():
-            if len(self.phase_images[phase]) > 0 and self.phase_images[phase][index] == image:
-                is_work = index in self.phase_work_images[phase]
-                phase.set_image(image, is_work)
+        self.set_image(phase, new_index)
     def _run(self, command):
+        """Run an external command"""
         done = subprocess.run(command)
         return done.returncode == 0
+    def _find_category(self, image_path):
+        """Figure out the (narrowest) category an image is in"""
+        correct_category = None
+        for category in self.categories:
+            if str(image_path).startswith(str(category.path)):
+                if correct_category is None or len(str(category.path)) > len(str(correct_category.path)):
+                    correct_category = category
+        return correct_category
 
     # Button actions
     def next(self, phase, image):
         print("next")
-        return self._switch_index(phase, 1)
+        _, _, images, _ = self.phase_info(phase)
+        return self._switch_index(phase, 1, images)
     def prev(self, phase, image):
         print("prev")
-        return self._switch_index(phase, -1)
+        _, _, images, _ = self.phase_info(phase)
+        return self._switch_index(phase, -1, images)
     def next_work(self, phase, image):
         print("next_work")
-        return self._switch_work_index(phase, 1, self.phase_work_images[phase])
+        _, _, _, work_images = self.phase_info(phase)
+        return self._switch_index(phase, 1, work_images)
     def prev_work(self, phase, image):
         print("prev_work")
-        return self._switch_work_index(phase, -1, self.phase_work_images[phase])
+        _, _, _, work_images = self.phase_info(phase)
+        return self._switch_index(phase, -1, work_images)
     def tag(self, tag, phase, image):
         print("tag", tag)
-        before = { phase: image.match_tags(tags) for phase, tags in self.phase_tags.items() }
-        before_index = { phase: self.phase_images[phase].index(image) for phase, truth in before.items() if truth == True }
+        before = { phase: image.match_tags(tags) for phase, tags, _, _, _ in self.phases() }
         image.tag(tag)
-        after  = { phase: image.match_tags(tags) for phase, tags in self.phase_tags.items() }
-        for phase in self.phase_tags:
-            if after[phase] == True and before[phase] == False:
+        after  = { phase: image.match_tags(tags) for phase, tags, _, _, _ in self.phases() }
+        for phase, tags, phase_index, images, work_images in self.phases():
+            if before[phase] == False and after[phase] == True:
                 # Added to phase
-                index = self.phase_images[phase].index(image)
-                self.phase_work_images.append()
+                work_images.append(image.index)
                 phase.increment_todo(1)
                 phase.increment_skipped(-1) # Usually it's skipped and not finished, but this is a guess
-                if len(self.phase_work_images) == 1: # New first image
-                    self._update_image(phase)
-                    phase.set_done(False)
+                if len(work_images) == 1: # New first image
+                    self.set_image(phase, 0)
             elif before[phase] == True and after[phase] == False:
                 # Removed from phase.
-                index = before_index[phase]
                 phase.increment_todo(-1)
                 phase.increment_finished(1)
-                if self.phase_index[phase] == index: # advance the cursor, too
+                if phase_index == image.index: # advance the cursor, too
                     self.next_work(phase, image)
-                self.phase_work_images[phase].remove(index)
-                if len(self.phase_work_images[phase]) == 0:
+                work_images.remove(image.index)
+                if len(work_images) == 0:
                     self.autoselect_phase()
                     phase.set_done(True, popup=True)
     def rotate_left(self, _, image):
         print("rotate_left")
         self._run(["convert", image.image_path, "-rotate", "270", image.image_path])
-        self._reload_image(image)
+        self.reload_image(image)
     def rotate_right(self, _, image):
         print("rotate_right")
         self._run(["convert", image.image_path, "-rotate", "90", image.image_path])
-        self._reload_image(image)
+        self.reload_image(image)
     def crop(self, _, image):
         print("crop")
         success = self._run(["cropgui", image.image_path]) # Only works on jpg
         if success:
             new_path = image.image_path.parent.joinpath("{}-crop{}".format(image.image_path.stem, image.image_path.suffix))
             os.rename(new_path, image.image_path)
-            self._reload_image(image)
+            self.reload_image(image)
     def set_category(self, phase, image):
         print("set_category")
         category = phase.get_category()
